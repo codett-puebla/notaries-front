b171570a806b78c297468481f36969ce64f7e795
Alejandro <alexrodriguezserrano1595@gmail.com>
2019-07-20 15:11:25 -0500

    install bootstrap and jquery and generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SideBarComponent } from './components/shared/side-bar/side-bar.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent,
+    SideBarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
