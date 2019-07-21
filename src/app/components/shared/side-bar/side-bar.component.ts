aeac995504853962b7a085c141d41bd28ffa953d
Alejandro <alexrodriguezserrano1595@gmail.com>
2019-07-20 23:46:36 -0500

    external module

--- src/app/components/shared/side-bar/side-bar.component.ts
+++ src/app/components/shared/side-bar/side-bar.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  const linkCURP = 'https://consultas.curp.gob.mx/CurpSP/gobmx/inicio.jsp';
-  const linkRFC = 'https://portalsat.plataforma.sat.gob.mx/ConsultaRFC/';
-  const linkNewRFC = 'https://www.sat.gob.mx/tramites/33629/realiza-tu-inscripcion-en-el-rfc-persona-fisica';
-  const linkISABI = 'https://portalsfa.sfapuebla.gob.mx/srl/Impuestos/Notarios/Requisitos.jsp';
+  linkCURP = 'https://consultas.curp.gob.mx/CurpSP/gobmx/inicio.jsp';
+  linkRFC = 'https://portalsat.plataforma.sat.gob.mx/ConsultaRFC/';
+  linkNewRFC = 'https://www.sat.gob.mx/tramites/33629/realiza-tu-inscripcion-en-el-rfc-persona-fisica';
+  linkISABI = 'https://portalsfa.sfapuebla.gob.mx/srl/Impuestos/Notarios/Requisitos.jsp';
 
   @Input() toggle;
   constructor() { }
