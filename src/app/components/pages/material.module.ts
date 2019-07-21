3c2fe2b15c4eb598804bf1d2d88424310ed7c35a
Hugo Daniel <mahgt_34@hotmail.com>
2019-07-20 19:36:58 -0500

    update angularcli

--- src/app/components/pages/material.module.ts
+++ src/app/components/pages/material.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CotizacionComponent} from './cotizacion/cotizacion.component';
+import {MatAutocompleteModule, MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [CotizacionComponent],
+  exports: [
+    CotizacionComponent
+  ],
   imports: [
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
     CommonModule
   ]
 })
-export class MaterialModule { }
+export class MaterialModule {
+}
