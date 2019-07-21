629ae00e6d92579629e24693f69236ee4679e34a
Hugo Daniel <mahgt_34@hotmail.com>
2019-07-20 19:42:17 -0500

    update

---
+++ src/app/components/pages/cotizacion/cotizacion.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-cotizacion',
+  templateUrl: './cotizacion.component.html'
+})
+export class CotizacionComponent implements OnInit {
+  myForm: FormGroup;
+  inputTramite = new FormControl();
+  // opciones a seleccionar
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+  arrayCost: any = ['Honorario'];
+
+  constructor() {
+    this.myForm = new FormGroup(
+      {
+        inputTramite: this.inputTramite,
+        arrayCost: new FormArray([
+          new FormControl('')
+        ])
+      }
+    )
+    ;
+  }
+
+  ngOnInit() {
+    this.filteredOptions = this.inputTramite.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  // metodo de filtrado
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  submit() {
+    console.log(this.myForm);
+  }
+
+  addNewCost() {
+    console.log('añadiendo');
+    (this.myForm.controls.arrayCost as FormArray).push(new FormControl());
+  }
+}
